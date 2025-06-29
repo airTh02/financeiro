bfb878caeb51005c913d26d1eeeb278432da2e73
th <thiagorerezende@gmail.com>
2025-06-29 09:56:19 -0300

    salvando

---
+++ src/components/TableItem/index.tsx
@@ -0,0 +1,18 @@
+import { formatDate } from '../../helpers/dateFilter'
+import { Item } from '../../types/Item'
+import * as C from './styles'
+
+type Props = {
+    item: Item
+}
+
+export const TableItem = ({item}: Props) => {
+    return (
+        <C.TableLine>
+            <C.TableColumn>{formatDate(item.date)}</C.TableColumn>
+            <C.TableColumn>{item.category}</C.TableColumn>
+            <C.TableColumn>{item.title}</C.TableColumn>
+            <C.TableColumn>R$ {item.value}</C.TableColumn>
+        </C.TableLine>
+    )
+}
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const Items:Item[] = [
+    { date: new Date(2025, 5, 25), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2025, 5, 25), category: 'food', title: 'Burger King', value: 32.12 },
+    { date: new Date(2025, 5, 26), category: 'rent', title: 'Aluguel Apt', value: 32.12 },
+    { date: new Date(2025, 6, 12), category: 'salary', title: 'Salário ACME', value: 4500 }
+]
