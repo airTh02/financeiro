6ebddac2f3f532f345b7c57cc4914878ab93092b
thiago <thiagorerezende@gmail.com>
2025-08-07 17:49:01 -0300

    feito

---
+++ src/components/ResumeItem/index.tsx
@@ -0,0 +1,17 @@
+import * as C from './styles'
+
+
+type Props = {
+    title: string
+    value: number;
+    color?: string;
+}
+
+export const ResumeItem = ({value, title, color}: Props) => {
+    return(
+        <C.Container>
+            <C.Title>{title}</C.Title>
+            <C.Info color={color}>R${value.toFixed(2)}</C.Info>
+        </C.Container>
+    )
+}
